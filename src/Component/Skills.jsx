0c30f3f02988c0a25dce3c3d2b58f5691d753a34
Mohamed Rami <101269704+mohamedramii@users.noreply.github.com>
2023-02-01 16:05:15 +0200

    Update Skills.jsx

--- src/Component/Skills.jsx
+++ src/Component/Skills.jsx
@@ -16,8 +16,7 @@ const Skills = () => {
     <div name='skills' className=' w-full h-full bg-[#0a192f] text-gray-300'>
       {/* Container */}
       <div
-        className=' border-2
-         border-sky-500 max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full '>
+        className='  max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full '>
         <div>
           <p className='text-4xl font-bold inline border-b-4 border-pink-600  '>
             Experience
@@ -26,8 +25,7 @@ const Skills = () => {
         </div>
 
         <div
-          className='  border-2
-         border-sky-500 w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+          className='   w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
             <img className='w-20 mx-auto' src={HTML} alt='HTML icon' />
             <p className='my-4'>HTML</p>
