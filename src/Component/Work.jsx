d361ce17bbb95b0b78fb55f4ca9b28957c6b935d
Mohamed Rami <101269704+mohamedramii@users.noreply.github.com>
2023-02-01 16:05:38 +0200

    Update Work.jsx

--- src/Component/Work.jsx
+++ src/Component/Work.jsx
@@ -5,8 +5,7 @@ const Work = () => {
   return (
     <div
       name='work'
-      className='border-2
-         border-sky-500 w-full md:h-full bg-[#0a192f] text-gray-300'>
+      className=' w-full md:h-full bg-[#0a192f] text-gray-300'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
