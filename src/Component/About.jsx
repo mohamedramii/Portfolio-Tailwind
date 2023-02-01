112aee8c7a8a6c54d7e4fc79a58df3d5ecd069dd
Mohamed Rami <101269704+mohamedramii@users.noreply.github.com>
2023-02-01 16:04:22 +0200

    Update About.jsx

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -4,8 +4,8 @@ const About = () => {
   return (
     <div
       name='about'
-      className=' bg-[#0a192f] w-full h-screen border-2 border-sky-500 text-gray-300'>
-      <div className='border-2 border-sky-100 flex flex-col justify-center items-center w-full h-full'>
+      className=' bg-[#0a192f] w-full h-screen  text-gray-300'>
+      <div className=' flex flex-col justify-center items-center w-full h-full'>
         <div className='max-w-[1000px] w-full  grid grid-cols-2 gap-8'>
           <div className='sm:text-right pb-8 pl-4'>
             <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
